0da195b24f41eab7d25e2d1c5d5dcd1dca85ee44
johncabang <johncabz@gmail.com>
2020-10-19 20:09:52 +0000

    Update menu_seeds

--- db/seeds/02_menu_seeds.sql
+++ db/seeds/02_menu_seeds.sql
@@ -1,12 +1,13 @@
 INSERT INTO menu_items (name, description, picture, price, restaurant_id)
-VALUES ('Matcha Coconut Panna Cotta', 'This Coconut Matcha Panna Cotta is based on a traditional panna cotta but uses coconut milk and cream to replace the dairy.', '../images/-.jpg', 1000, 1),
-('Cherry Cheesecake', 'Vanilla cake, filled with cherry filling and topped with cream cheese butter cream.', '../images/cherrycheesecake.jpg', 1400, 1),
-('Creme Brulee', 'Classic French dessert consisting of a custard topped with caramelized sugar.', '../images/cremebrulee.jpg', 2000, 1),
-('Rosewater Panna Cotta', 'A creamy, elegant Italian dessert flavored with rose water.', '../images/rosewaterpannacotta.jpg', 1100, 1),
+VALUES ('Matcha Coconut Panna Cotta', 'This Coconut Matcha Panna Cotta is based on a traditional panna cotta but uses coconut milk and cream to replace the dairy.', '../images/matcha.jpeg', 2000, 1),
+('Cherry Cheesecake', 'Vanilla cake, filled with cherry filling and topped with cream cheese butter cream.', '../images/Cherry Cheesecake slices.jpg', 1400, 1),
+('Creme Brulee', 'Classic French dessert consisting of a custard topped with caramelized sugar.', '../images/Creme-Brulee.jpg', 2000, 1),
+('Rosewater Panna Cotta', 'A creamy, elegant Italian dessert flavored with rose water.', '../images/Rosewater Panna Cotta _ Queen Fine Foods.jpg', 1800, 1),
+
 ('Lemon Tart Chestnut and Figs', 'This recipe for lemon tart, chestnut and Corsican figs is a mixture of sweetness and acidity.', '../images/lemontart.jpg', 1500, 1),
 ('Violet Mousse with Blueberry', 'Contemporary Blueberry Violet Mousse Cake covered with velvet spray and decorated with chocolate elements, on white background.', '../images/violetmousse.jpg', 2200, 1),
-('Lava Cake', 'Molten Peanut Butter and Chocolate Fondant Cakes', '../images/lavacake.jpg', 1700, 1),
-('Raspberry Chocolate Cake', 'Tender chocolate cake is layered with raspberry jam and rich chocolate.', '../images/raspberrychocolatecake.jpg', 1500, 1);
+('Dessert Platter', ' The Signature Dessert Platter contains an abundance of freshly baked gourmet pastries and desserts.', '../images/dessertplatter.jpg', 4500, 1),
+('Cherry Cake', 'The secret in this recipe is undiluted evaporated milk for added richness.', '../images/cherrycake.jpg', 1500, 1);
 
 
 INSERT INTO restaurants (name, phone)
