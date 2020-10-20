19aafa7d6927b1f6677f9532950881b954128ccf
johncabang <johncabz@gmail.com>
2020-10-20 15:30:42 +0000

    Add link to cart, get to cart inside server.js

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
