00d5c8f28ae9db35d8cc2180cad750e8d197ad12
Your Name <akas3524@yahoo.com>
2020-10-23 03:36:58 +0000

    twilio stuff

--- apiHelper/twilio.js
+++ apiHelper/twilio.js
@@ -1,12 +1,26 @@
-var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Your Account SID from www.twilio.com/console
-var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+const textSender = (body, receiver) => {
 
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
+  const accountSid = process.env.accountSid; // Your Account SID from www.twilio.com/console
+  const authToken = process.env.authToken;   // Your Auth Token from www.twilio.com/console
+  const from = process.env.fromNumber
+  const client = require('twilio')(accountSid, authToken);
+  return client.messages.create({ body, from, receiver })
+    .then(res => console.log(res.body));
+}
 
-client.messages.create({
-    body: 'Hello from Node',
-    to: '+12345678901',  // Text this number
-    from: '+12345678901' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+const textToAdmin = (orders_id) => {
+  const adminNumber = process.env.adminNumber;
+  let message = `order number ${orders_id} is placed. Please check the Admin side to check the order!`;
+  return textSender(message, adminNumber);
+}
+
+// client.messages.create({
+//   body: 'Hello from Node',
+//   to: '+12345678901',  // Text this number
+//   from: '+12345678901' // From a valid Twilio number
+// })
+//   .then((message) => console.log(message.sid));
+
+
+module.exports = { textSender, textextToAdmin };
+
